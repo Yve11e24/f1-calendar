f56fca939c2223531f3c053df237ae7f5f18504c
arzn <tyxxx921@gmail.com>
2026-09-03 17:55:19 +0800

    Day 7: generate F1 calendar ICS

--- f1_calendar.py
+++ f1_calendar.py
@@ -3,6 +3,15 @@ from datetime import datetime, timezone, timedelta
 
 taiwan_timezone = timezone(timedelta(hours=8))
 
+session_durations = {
+    "FirstPractice": timedelta(hours=1),
+    "SecondPractice": timedelta(hours=1),
+    "ThirdPractice": timedelta(hours=1),
+    "Sprint": timedelta(hours=1),
+    "Qualifying": timedelta(hours=1),
+    "Race": timedelta(hours=2)
+}
+
 def get_f1_races():
     url = "https://api.jolpi.ca/ergast/f1/2026.json"
 
@@ -28,6 +37,33 @@ def convert_to_tw_time(session):
 
         return tw_time
 
+def format_ics_datetime(dt):
+     utc_time = dt.astimezone(timezone.utc)
+     return utc_time.strftime("%Y%m%dT%H%M%SZ")
+
+def create_ics(session):
+     lines = []
+     lines.append("BEGIN:VCALENDAR")
+     lines.append("VERSION:2.0")
+     lines.append("PRODID:-//F1 Calender//TW//")
+     lines.append("CALSCALE:GREGORIAN")
+     lines.append("METHOD:PUBLISH")
+
+     for session in session:
+          start = format_ics_datetime(session["start"])
+          end = format_ics_datetime(session["end"])
+
+          lines.append("BEGIN:VEVENT")
+          lines.append(
+               f"SUMMARY:{session['race']} - {session['name']}"
+          )
+          lines.append(f"DTSTART:{start}")
+          lines.append(f"DTEND:{end}")
+          lines.append("END:VEVENT")
+
+     lines.append("END:VCALENDAR")
+     return "\n".join(lines)
+
 races = get_f1_races()
 print("比賽總數:",len(races))
 
@@ -39,39 +75,69 @@ session_names = [
     "Qualifying",
 ]
 
+all_sessions = []
+
 for race in races:
      print("\n", race["raceName"])
 
      sessions = []
      for session_name in session_names:
           if session_name in race:
-               tw_time = convert_to_tw_time(
+               start_time = convert_to_tw_time(
                     race[session_name]
                )
+
+               end_time = (
+                    start_time
+                    + session_durations[session_name]
+               )
+
                session = {
                     "race": race["raceName"],
                     "name": session_name,
-                    "time": tw_time
+                    "start": start_time,
+                    "end": end_time
                }
                sessions.append(session)
+               all_sessions.append(session)
 
      race_session = {
          "date": race["date"],
          "time": race["time"]
      }
 
-     tw_time = convert_to_tw_time(race_session)
+     start_time = convert_to_tw_time(race_session)
+     end_time = (
+          start_time
+          + session_durations["Race"]
+     )
 
      session = {
           "race": race["raceName"],
           "name": "Race",
-          "time": tw_time
+          "start": start_time,
+          "end": end_time
      }
      sessions.append(session)
+     all_sessions.append(session)
 
      for session in sessions:
          print(
             session["name"],
             "→",
-            session["time"]
+            session["start"],
+            "到",
+            session["end"]
          )
+
+ics_content = create_ics(all_sessions)
+with open(
+     "f1_2026.ics",
+     "w", 
+     encoding="utf-8"
+) as file:
+     file.write(ics_content)
+
+print("\n已建立 f1_2026.ics")
+print("總是件數:", len(all_sessions))
+
